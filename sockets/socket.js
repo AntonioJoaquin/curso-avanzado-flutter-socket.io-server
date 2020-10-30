e05bb4322a02991bf2f653cc77ab64c076accfa6
Antonio Joaquin <antoniojoaquinluque04@gmail.com>
2020-10-30 11:05:05 +0100

    - Sección 4 - Clase 37 -

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,20 +1,38 @@
 const { io } = require('../index');
 
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+
+const bands = new Bands();
+
+bands.addBand(new Band('Mago de Öz'));
+bands.addBand(new Band('Saurom'));
+bands.addBand(new Band('Lépoca'));
+bands.addBand(new Band('SFDK'));
+
+
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
 
-    client.on('mensaje', (payload) => {
-        console.log('Mensaje ', payload);
 
-        io.emit('mensaje', { admin: 'Nuevo mensaje' });
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+
+        io.emit('active-bands', bands.getBands());
     });
 
-    client.on('emitir-mensaje', (payload) => {
-        io.emit('nuevo-mensaje', payload);
+    client.on('add-band', (payload) => {
+        const newBand = new Band(payload.newBandName);
+        bands.addBand(newBand);
+
+        io.emit('active-bands', bands.getBands());
     });
 });
